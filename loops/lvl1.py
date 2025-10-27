f8ed2521abd4168df467e4125fe5592e52d77f5b
pygod139 <rubinbas7@gmail.com>
2025-10-27 09:28:36 +0545

    added sovled qn

--- loops/lvl1.py
+++ loops/lvl1.py
@@ -5,7 +5,7 @@
 """
 #2. Print numbers from 10 to 1 using `range(start, stop, step)`.
 
-def is_prime(num):
+"""def is_prime(num):
     sqr = int(num ** 0.5) #finding the sqr root.
     for i in range(2, sqr+1):
         if num%i == 0:
@@ -20,3 +20,25 @@ for i in range(10, 1, -1):
         print(i)
     else:
         pass
+"""
+
+"""#3. Print every 2nd number from 2 to 20.
+
+for i in range(2,21, 2):
+    print(i)"""
+
+"""#4. Print all even numbers between 1 and 40 using step size in `range()`.
+
+for i in range(2, 41, 2):
+    print(i)"""
+
+"""#5. Print the square of each number from 1 to 10.
+
+for i in range(1, 11):
+    print(i**i)"""
+
+#6. Print each element and its index from `[10, 20, 30, 40]` using `enumerate()`.
+
+nums = [10, 20, 30, 40]
+for po, va in enumerate(nums):
+    print(po, va)
