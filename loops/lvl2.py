f923bedf67389cb620ae07f163e812022d67cc4e
pygod139 <rubinbas7@gmail.com>
2025-10-29 10:35:36 +0545

    solved 2nd q

--- loops/lvl2.py
+++ loops/lvl2.py
@@ -1,7 +1,14 @@
 #11. Print numbers from 1–15 but skip 5 using `continue`.
 
-for i in range(1, 16):
+"""for i in range(1, 16):
     if i==5:
         continue
+    print(i)"""
+    
+#12. Print numbers from 1–10 but stop when 7 is reached using `break`.
+
+for i in range(1, 11):
+    if i==7:
+        break
     print(i)
-        
+
