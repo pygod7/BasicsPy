17572f13cdfd7d0c6b1983a237dc53924ef3d37e
pygod139 <rubinbas7@gmail.com>
2025-10-29 10:39:10 +0545

    solved for else stuff

--- loops/lvl2.py
+++ loops/lvl2.py
@@ -7,8 +7,15 @@
     
 #12. Print numbers from 1–10 but stop when 7 is reached using `break`.
 
-for i in range(1, 11):
+"""for i in range(1, 11):
     if i==7:
         break
     print(i)
+"""
 
+#13. Use `for...else` to print `"Loop completed"` after finishing.
+
+for i in range(5):
+    pass
+else:
+    print("Loop completed!")
