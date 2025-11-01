f60b5c974ebaeee20669815bdb3354b16248be69
pygod139 <rubinbas7@gmail.com>
2025-11-01 09:00:49 +0545

    Solved some problems!

--- loops/lvl3.py
+++ loops/lvl3.py
@@ -1,6 +1,60 @@
 #21. Print each key and value from:
 
-fruit_prices = {"apple": 100, "banana": 40, "cherry": 200}
+"""fruit_prices = {"apple": 100, "banana": 40, "cherry": 200}
 
 for k, v in fruit_prices.items():
-    print(k, v)
+    print(k, v)"""
+
+#22. Create a new list with squares of `[1, 2, 3, 4, 5]` using a loop.
+
+"""nums = [1, 2, 3, 4, 5]
+sqrnum = list()
+
+for i in nums:
+    sqrnum.append(i**2)
+print(sqrnum)"""
+
+#23. Print elements of `[[1, 2], [3, 4], [5, 6]]` one by one using nested loops.
+
+"""nested_list = [[1, 2], [3, 4], [5, 6]]
+
+syntax : 
+print(nested_list[0][0])
+print(nested_list[0][1])
+print(nested_list[1][0])
+
+for i in range(len(nested_list)):
+    foo =  nested_list[i]
+    for j in range(len(foo)):
+        print(foo[j])"""
+
+#24. Print all pairs `(i, j)` where `1 ≤ i, j ≤ 3`.
+# i dont understand qn, i'll solve by understanding qn via gpt once electricity comes.
+
+"""25. Print this pattern:
+*
+**
+***
+****
+*****
+
+"""
+
+"""for i in range(5):
+    print("*" * i, end="\n")
+
+"""
+
+"""26. Print this inverted pattern:
+*****
+****
+***
+**
+*
+
+"""
+
+"""for i in range(5, 0, -1):
+    print("*"*i, end="\n")"""
+
+
